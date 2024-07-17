44e14963e4291d0f7afc6aa371df2e40e9dd6c68
Tony Powell <powell.anthonyd@proton.me>
2024-07-17 14:33:20 -0400

    Strip extraneous links

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,8 +7,8 @@ import {
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Clerk Login Bug Reproduction" },
+    { name: "description", content: "Clerk Login Bug Reproduction" },
   ];
 };
 
@@ -25,40 +25,9 @@ export const loader = async (args: LoaderFunctionArgs) => {
 export default function Index() {
   return (
     <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
       <ul className="list-disc mt-4 pl-6 space-y-2">
         <li>
           <a href="/sign-in">Sign In</a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
         </li>
       </ul>
     </div>
